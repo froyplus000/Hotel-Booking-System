30b59b995cdf7df94167ed1d7238c8b7811b718c
Pattarapol Tantechasa <froyplus@hotmail.co.th>
2025-12-01 01:49:09 +1100

    feat: docker containerized api and worker #1

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,11 +12,18 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// Keep Swagger always on for learning purpose. Will turn off later when working with production on AWS
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Health check endpoint 
+app.MapGet("/health", () => Results.Ok(new {status = "ok"}));
 
 app.UseHttpsRedirection();
 
--- src/Worker/Program.cs
+++ src/Worker/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = Host.CreateApplicationBuilder();
+
+// Basic Logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Worker");
+logger.LogInformation("Hotel Booking Worker Starting Up.....");
+
+app.Services.GetRequiredService<IHostApplicationLifetime>().ApplicationStarted.Register(() =>
+{
+    _ = Task.Run(async () =>
+    {
+        while (true)
+        {
+            logger.LogInformation("Worker hearbeat at: {time}", DateTimeOffset.Now);
+            await Task.Delay(TimeSpan.FromSeconds(30));
+        }
+    });
+});
+
+await app.RunAsync();   
